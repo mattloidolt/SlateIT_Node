5cdffab4bf82881b095dbe34b1f8f8b53b93213e
Matt Loidolt <Loidolt@Matts-MacBook.local>
2015-10-12 17:49:56 -0600

    adding correct variables in the error handler

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
 
 /* GET login page. */
 router.get('/', function(req, res) {
-    res.render('login', { title: 'Express' });
+    res.render('login', {});
 });
 
 /* GET home page. */
 router.get('/home', function(req, res) {
-    res.render('home', { title: 'Express' });
+    res.render('home', {});
 });
 
 /* handle the request for information */
 router.get('/request', function (req, res) {
     var requestHandle = require('requestHandler');
     requestHandle.handleRequest(req, res);
+});
+
+/* handle the unrecognized routes */
+router.get('*', function (req, res) {
+    res.render('error', { "error": {"status": "404", "stack": "Page Not Found"}});
 });
 
 module.exports = router;
